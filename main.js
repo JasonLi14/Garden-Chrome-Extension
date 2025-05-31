079f728f8916b501e767db2ec3b7e7ef7203468d
Jason <jiachengfriend@gmail.com>
2025-05-31 14:12:07 -0600

    Set up the project, make grass

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
+import * as PLANTS from "./src/plants.js"
+
 // constants
-const app = new PIXI.Application();
 const WIDTH = 640;
 const HEIGHT = 360;
 
-// test
-let obj = new PIXI.Graphics()
-  .rect(0, 0, 200, 100)
-  .fill(0xff0000);
+const grass = new PLANTS.Grass();
+grass.makeData();
+grass.makeGraphic();
+const grass_graphic = grass.getGraphic();
+grass_graphic.scale = 0.5;
+grass_graphic.rotation = Math.PI;
+grass_graphic.x = 100;
+grass_graphic.y = 100;
+// Initialize the app
+const app = new PIXI.Application();
 
-await app.init({ width: WIDTH, height: HEIGHT });
-app.stage.addChild(obj);
-console.log("Hello World");
+await app.init({
+  backgroundColor: 0xaabbff, 
+  width: WIDTH, 
+  height: HEIGHT, 
+  antialias: true,
+  resolution: 1 });
+
+app.stage.addChild(grass_graphic);
 
 document.body.append(app.canvas);
 
+// Animation
+// Count seconds that it has been running
+let elapsed = 0.0;
+app.ticker.add((ticker) => {
+  // Add the time to our total elapsed time
+  elapsed += ticker.deltaTime;
+  // test movement
+  // grass.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
+});
---
+++ src/plants.js
@@ -0,0 +1,79 @@
+// Constants
+const WIDTH = 640;
+const HEIGHT = 360;
+const SEED = 0
+
+export function Plant() {
+    this.size = 100;
+    this.growth = 0;
+    this.data_index = 0;
+    this.data = [];
+    this.info = {};
+
+    // For loading saved data about plants
+    this.loadData = function(data) {
+        this.data = data;
+    }
+
+    // For generating data for a plant based on some info
+    this.makeData = function() {}
+
+    // For creating the actual graphic
+    this.makeGraphic = function(growth=1) {} 
+
+    // 
+    this.getGraphic = function() {
+        return this.graphic;
+    }
+
+    // initialize graphic
+    this.graphic = new PIXI.Container({
+        x:0,
+        y:0
+      });
+    
+}
+
+export function Grass(size=100, min_amt=10) {
+    Plant.call(this, size);
+    
+    this.min_amt = min_amt;
+
+    // create data for grass
+    this.makeData = function() {
+        // find amount of grass
+        const amount = Math.floor(Math.random() * this.min_amt + this.min_amt);
+        let y = 0;
+        let previous = 0;
+
+        for (let i = 0; i < amount; i++) {
+            // a blade of grass
+            const blade_width = this.size * Math.random() / 5;
+            const blade_height = this.size / 2 * Math.random() + this.size;
+            const blade_offset = blade_width * Math.random();
+            this.data.push([previous, y, previous + blade_offset, y + blade_height, previous + blade_width, y]);
+
+            // update previous
+            previous = previous + blade_offset;
+            
+        }
+    }
+
+    // Draw the grass graphic
+    this.makeGraphic = function(growth=1) {
+        // create the object from data
+        const data_length = this.data.length;
+        for (let i = 0; i < data_length * growth; ++i) {
+            const blade = new PIXI.Graphics()
+                .poly(this.data[i])
+                .fill(0x00aa00);          
+            this.graphic.addChild(blade);
+        }
+        
+    }
+}
+
+export function Tree() {
+
+}
+
